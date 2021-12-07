108d0d4b570d9435af35aee5f2c6200c44a36c40
saidabbasun <saidabboscoder0000@gmail.com>
2021-12-07 09:59:57 +0500

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=7770cad617ee1ab8bff0&state=6d6effe7-af96-40b9-95a3-5d642991f6ca
    Signed-off-by: saidabbasun <saidabboscoder0000@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { useState } from "react";
+import Field from "./components/field";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
 
-function App() {
+export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Field label="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
-
-export default App;
